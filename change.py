22e64b0505433ffc7845aeae766906b9dd2407cc
victoria-rivaspa13 <162721841+victoria-rivaspa13@users.noreply.github.com>
2024-04-02 17:38:03 -0300

    Update change.py

--- change.py
+++ change.py
@@ -3,3 +3,12 @@ def change():
     money = 100
 print("Ingresar gasto:")
 print(expense)
+print("Dinero recibido")
+print(money)
+print()
+print("Vuelto")
+print()
+print("Pesos:")
+print(intmoney - expense))
+print("Centavos:")
+print(int(int(float(money-expense)-int(money-expense))*100))
