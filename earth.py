88fbc254c6fafdbc2c7f439ffaded097eb5b84ea
victoria-rivaspa13 <162721841+victoria-rivaspa13@users.noreply.github.com>
2024-04-02 17:46:35 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,3 +1,6 @@
 def earth():
-    x = "Bangladesh"
-    y = "Barbados"
+    X= "Bangladesh"
+    Y= "Barbados"
+    print("The result of "+ X +" comes first in the directory than "+ Y +" is "+ str(Y > X) + ".")
+    print("The result of "+ Y +" comes first in the directory than "+ X +" is "+ str(X > Y) + ".")
+    
